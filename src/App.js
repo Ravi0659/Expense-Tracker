fa9b831c3b218106b9cb561674a6c3abd4095745
Ravi <ravi.kr6075@gmail.com>
2024-12-29 15:38:52 +0530

    abc

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Dashboard from './component/Dashboard/Dashboard';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Expense Tracker</h1>
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
